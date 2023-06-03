44151d7907113266403da65ce21f8f5191725b2a
aozp73 <aozp73@naver.com>
2023-06-03 16:26:42 +0900

    initial commit 2 (해당 프로젝트에선 백엔드 개발만 진행)

---
+++ Comorize/src/main/java/com/cos/comorizestart/ComorizeStartApplication.java
@@ -0,0 +1,13 @@
+package com.cos.comorizestart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComorizeStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComorizeStartApplication.class, args);
+	}
+
+}
