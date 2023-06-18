6e85e0390824ecdcadc68a03604d161387d09b79
aozp73 <aozp73@naver.com>
2023-06-18 11:55:01 +0900

    구독하기 - Query 완성 후 Server에 조회 로직 (qlrm, DTO 매핑)

--- Comorize/src/main/java/com/cos/comorizestart/service/SubscribeService.java
+++ Comorize/src/main/java/com/cos/comorizestart/service/SubscribeService.java
@@ -2,6 +2,10 @@ package com.cos.comorizestart.service;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.qlrm.mapper.JpaResultMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,11 +20,30 @@ import lombok.RequiredArgsConstructor;
 public class SubscribeService {
 
 	private final SubscribeRepository subscribeRepository;
+	private final EntityManager em;
 	
 	@Transactional(readOnly = true)
 	public List<SubscribeDTO> 구독리스트(int principalId, int pageUserId){
 		
-		return null;
+		// Query 준비
+		StringBuffer sb = new StringBuffer();
+		sb.append("select u.id userId, u.username, u.profileImageUrl,  ");
+		sb.append("if( (select true from subscribe where fromUserId = ? and toUserId = u.id), true, false) subscribeState, ");  // principalDetails.user.id
+		sb.append("if(u.id = ?, true, false) equalState "); // principalDetails.user.id
+		sb.append("from subscribe f inner join user u on u.id = f.toUserId ");
+		sb.append("where f.fromUserId = ? "); // pageUserId
+		
+		// Query 완성
+		Query query = em.createNativeQuery(sb.toString())
+				.setParameter(1, principalId)
+				.setParameter(2, principalId)
+				.setParameter(3, pageUserId);
+		
+		// Query 실행 (이 부분에서 qlrm 라이브러리 활용: DTO에 DB조회 결과 매핑하기 위해)
+		JpaResultMapper result = new JpaResultMapper();
+		List<SubscribeDTO> subscribeDTOs = result.list(query, SubscribeDTO.class);
+		
+		return subscribeDTOs;
 	}
 	
 	
